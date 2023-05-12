cd1355c3cb6910078caba1a580d91598a8228dec
Shogo Kitada <contact@shogo0x2e.com>
2023-05-12 23:59:57 +0900

    shibalab.com に実装した API は全部モジュール化した (api_handlers.js)

--- api_handlers.js
+++ api_handlers.js
@@ -11,21 +11,41 @@
 
 const apiUrl = "https://digicre.shibalab.com/event/2023-omiya";
 
-// 来場者数を取得する関数
+/**
+ * 来場者数を取得する関数
+ * 
+ * @param {string} visitorId 読み取った作品の ID
+ * @return 作品の来場者数
+ */
 export async function getVisitorCount(visitorId) {
   const response = await fetch(`${apiUrl}/?visitor=${visitorId}`);
   const data = await response.json();
   return data.visitor_count;
 }
 
-// 作品情報を取得する関数
+/**
+ * 作品情報を取得する関数
+ * 
+ * @param {string} artworkId 読み取った作品の ID
+ * @returns {{
+ *  title: string,  
+ *  introduction: string, 
+ *  image: string, 
+ * }}
+ */
 export async function getArtworkInfo(artworkId) {
   const response = await fetch(`${apiUrl}/?artwork=${artworkId}`);
   const data = await response.json();
   return data;
 }
 
-    
+/**
+ * 訪問者を記録する関数
+ * (localStorage による重複防止は呼び出し側で行う)
+ * 
+ * @param {string} artworkId 読み取った作品のID
+ * @returns {string} API からのメッセージ
+ */    
 export async function recordVisitor(artworkId) {
   const response = await fetch(`${apiUrl}/?visitor`, {
     method: 'POST',
